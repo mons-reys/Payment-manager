f2bea5ed6226ead20fbff34b8e616dad3eaf6e1a
mons-reys <mons.reys1@gmail.com>
2021-02-05 12:47:32 +0100

    added the invoice and payement scripts

--- public/app.js
+++ public/app.js
@@ -1,2 +1,20 @@
-"use strict";
-console.log("hello word");
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+const form = document.querySelector('form');
+const fromTo = document.querySelector('#fromTo');
+const details = document.querySelector('#details');
+const type = document.querySelector('#type');
+const amount = document.querySelector('#amount');
+let moc = new Invoice("mons", "hzloo", 20);
+console.log(moc);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(fromTo.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(fromTo.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,26 @@
-import {Invoice} from './classes/Invoice';
-import {Invoice} from './classes/Invoice';
+import {Invoice} from './classes/Invoice.js';
+import {Payment} from './classes/Payment.js';
+import {HasFormatter} from './interfaces/HasFormatter';
+
+const form = document.querySelector('form') as HTMLFormElement;
+const fromTo = document.querySelector('#fromTo') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const type = document.querySelector('#type') as HTMLSelectElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+
+let moc = new Invoice("mons", "hzloo", 20);
+console.log(moc);
+
+form.addEventListener('submit', (e:Event) =>{
+    e.preventDefault();
+    let doc;
+    if(type.value === "invoice"){
+        doc = new Invoice(fromTo.value, details.value, amount.valueAsNumber);
+    }else{
+        doc = new Payment(fromTo.value, details.value, amount.valueAsNumber);
+    }
+    
+
+   console.log(doc);
+})
