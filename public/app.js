dd6b798aae58686a0c2631719b7b4a5b6105ef47
mons-reys <mons.reys1@gmail.com>
2021-02-08 12:36:03 +0100

    added the header and the footer

--- public/app.js
+++ public/app.js
@@ -7,17 +7,26 @@ const details = document.querySelector('#details');
 const type = document.querySelector('#type');
 const amount = document.querySelector('#amount');
 //List template instance 
-const ul = document.querySelector('ul');
+const ul = document.querySelector('.actions');
 let list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
+    let values;
+    values = [fromTo.value, details.value, amount.valueAsNumber];
     if (type.value === "invoice") {
-        doc = new Invoice(fromTo.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(fromTo.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     ul.setAttribute('class', 'collection');
     list.render(doc, type.value, 'start');
 });
+//generics 
+const i = (obj) => {
+    let a = { ages: 20 };
+    return Object.assign(Object.assign({}, obj), a);
+};
+let test = i({ name: "mons", age: 20 });
+console.log(test.name, test.age, test.ages);
--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const type = document.querySelector('#type') as HTMLSelectElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
 //List template instance 
-const ul = document.querySelector('ul')!;
+const ul = document.querySelector('.actions') as HTMLUListElement;
 let list = new ListTemplate(ul);
 
 
@@ -18,14 +18,27 @@ let list = new ListTemplate(ul);
 form.addEventListener('submit', (e:Event) =>{
     e.preventDefault();
     let doc;
+    let values: [string, string, number];
+    values = [fromTo.value, details.value, amount.valueAsNumber];
     
     
     if(type.value === "invoice"){
-        doc = new Invoice(fromTo.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }else{
-        doc = new Payment(fromTo.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     
     ul.setAttribute('class', 'collection');
     list.render(doc, type.value, 'start');
 })
+
+
+//generics 
+
+const i = <T extends {name: string}> (obj: T) =>{
+    let a = {ages: 20};
+    return {...obj,...a}
+}
+
+let test = i({name: "mons", age: 20});
+console.log(test.name, test.age, test.ages);
